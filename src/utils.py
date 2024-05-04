dd0252399df57751a713d366848fca0bb1eef2ca
João Henrique Lopes <joaohenriquelopesdiv@gmail.com>
2024-05-03 22:29:30 -0300

    Fix: Concatenações & Símbolos

--- src/utils.py
+++ src/utils.py
@@ -21,30 +21,31 @@ def cadastrar_usuario():
         print("Informe o valor correto!!!")
         
 def preco_venda():
-    try:
-        nome_produto = input("Informe o nome do produto: ")
-        custo_do_produto = float(input("Informe o custo do produto em reais: "))
-        custo_administrativo = float(input("Informe o custo administrativo o produto em porcentagem: "))
-        comissao_de_vendas = float(input("Informe a comissão de venda do produto em porcentagem: "))
-        imposto = float(input("Informe os impostos em porcentagem: "))
-        margem_de_lucro = float(input("Informe a margem de lucro desejada em porcentagem: "))
-        desc = float(input("Informe a descrição: "))
-        print("_"*50)
-    except ValueError:
-        print("Informe o valor correto!!!")
-    preco_de_venda = custo_do_produto / ( 1 - (((custo_administrativo/100) + (comissao_de_vendas/100) + (imposto/100) + (margem_de_lucro/100)/ 1 )))
+    while True:
+        try:
+            nome_produto = input("Informe o nome do produto: ")
+            custo_do_produto = float(input("Informe o custo do produto em reais: "))
+            custo_administrativo = float(input("Informe o custo administrativo o produto em porcentagem: "))
+            comissao_de_vendas = float(input("Informe a comissão de venda do produto em porcentagem: "))
+            imposto = float(input("Informe os impostos em porcentagem: "))
+            margem_de_lucro = float(input("Informe a margem de lucro desejada em porcentagem: "))
+            desc = input("Informe a descrição: ")
+            print("_"*50)
+        except ValueError:
+            print("Informe o valor correto!!!")
+        preco_de_venda = custo_do_produto / ( 1 - (((custo_administrativo/100) + (comissao_de_vendas/100) + (imposto/100) + (margem_de_lucro/100)/ 1 )))
     
     print("\n")
     print("_"*50)
     print(f"Tabela de calculos de {nome_produto}")
     print("_"*50)    
-    print(f"A - Preço de venda é {preco_de_venda} ")
-    print(f"B - Custo de Aquisição é {custo_do_produto}")
-    print(f"C - Receita Bruta é {preco_de_venda - custo_do_produto}")
-    print(f"D - Custo Fixo é {(preco_de_venda)*(custo_administrativo/100)}")
-    print(f"E - Comissa de Vendas é {(preco_de_venda)*(comissao_de_vendas/100)}")
-    print(f"F - Impostos é {(preco_de_venda)*(imposto/100)}")
-    print(f"G - Outros Custos é {((preco_de_venda)*(imposto/100))+((preco_de_venda)*(comissao_de_vendas/100))+((preco_de_venda)*(custo_administrativo/100))}")
+    print(f"A - Preço de venda é R${preco_de_venda} ")
+    print(f"B - Custo de Aquisição é R${custo_do_produto}")
+    print(f"C - Receita Bruta é R${preco_de_venda - custo_do_produto}")
+    print(f"D - Custo Fixo é {(preco_de_venda)*(custo_administrativo/100)}%")
+    print(f"E - Comissão de Vendas é {(preco_de_venda)*(comissao_de_vendas/100)}%")
+    print(f"F - Impostos é {(preco_de_venda)*(imposto/100)}%")
+    print(f"G - Outros Custos é {((preco_de_venda)*(imposto/100))+((preco_de_venda)*(comissao_de_vendas/100))+((preco_de_venda)*(custo_administrativo/100))}%")
     print(f"H - Rentabilidade é {(preco_de_venda)*(margem_de_lucro/100)}")
     
     lucro_percentual = ((preco_de_venda - custo_do_produto) / preco_de_venda) * 100
@@ -66,6 +67,7 @@ def preco_venda():
     valores = (nome_produto, desc, custo_do_produto, custo_administrativo, comissao_de_vendas, imposto, margem_de_lucro)
     mycursor.execute(sql, valores)
     mydb.commit()
+    print(f"Adicionado com sucesso!")
     
     
     
